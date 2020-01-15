595b765d542b3d2fa54e9ede5a8926220bc5e60f
Aleksey <ekbdizzy@yandex.ru>
2020-01-15 17:48:06 +0300

    hot replace css and js

--- main/static/js/index.js
+++ main/static/js/index.js
@@ -1,8 +1,5 @@
 'use strict';
 
-const name = document.getElementById('name');
-const counter = document.getElementById('counter');
-let count = 0;
-
-name.innerText = 'and JS';
-setInterval(() => counter.innerText = ++count, 1000);
+import '../css/style.css';
+import './name';
+import './counter';
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,20 @@
 module.exports = {
     mode: 'development',
     entry: './main/static/js/index.js',
+    output: {
+        publicPath: "http://127.0.0.1:8080/"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+        ],
+    },
+    devServer: {
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
 };
